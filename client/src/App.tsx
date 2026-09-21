aecb619ef62331be18381032f0f4930a2d005035
Sourav Paul <sourav412p@gmail.com>
2026-09-21 13:59:38 +0530

    removed unused imports and style fixing for mobile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 
-//import './App.css';
 import { BrowserRouter as Router } from "react-router-dom";
-//import { Layout } from './layout/layout';
 import AppRoutes from './routes/app-routes';
 import { AuthProvider } from './context/authContext';
 import { ErrorBoundary } from "./components/common/ErrorBoundary";
